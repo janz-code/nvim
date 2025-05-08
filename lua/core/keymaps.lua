2dd868a697d56d8142b38306113e63e772e1e20c
JuanZuniga79 <cd.juanzuniga@gmail.com>
2025-05-07 21:52:13 -0500

    keymaps, settings, cmp, lspconfig, mason, neotree update. and conform, hover-nvim, lint-nvim, tailwind-colors, tailwindcss-colorizer, telescope, toggleterm, typescript-tools added

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -28,3 +28,38 @@ for i = 1, 9 do
 		require("bufferline").go_to(i)
 	end)
 end
+
+--- Conform
+
+vim.keymap.set("n", "<C-f>", function()
+	print(vim.g.mapleader)
+	require("conform").format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 1000,
+	})
+end, { desc = "Format file with conform" })
+
+--- Hover
+
+vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+vim.keymap.set("n", "<C-k>", require("hover").hover_select, { desc = "hover.nvim (select)" })
+vim.keymap.set("n", "<C-p>", function()
+	require("hover").hover_switch("previous")
+end, { desc = "hover.nvim (previous source)" })
+vim.keymap.set("n", "<C-n>", function()
+	require("hover").hover_switch("next")
+end, { desc = "hover.nvim (next source)" })
+vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
+vim.o.mousemoveevent = true
+
+--- Telescope
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>fa", function()
+	print("Leader space")
+end)
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,100 +1,100 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    {
-      "s1n7ax/nvim-window-picker",
-      version = "2.*",
-      config = function()
-        require("window-picker").setup({
-          filter_rules = {
-            include_current_win = false,
-            autoselect_one = true,
-            bo = {
-              filetype = { "neo-tree", "neo-tree-popup", "notify" },
-              buftype = { "terminal", "quickfix" },
-            },
-          },
-        })
-      end,
-    },
-  },
-  config = function()
-    vim.diagnostic.config({
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = " ",
-          [vim.diagnostic.severity.WARN] = " ",
-          [vim.diagnostic.severity.INFO] = " ",
-          [vim.diagnostic.severity.HINT] = " ",
-        },
-      },
-    })
-    require("neo-tree").setup({
-      source_selector = {
-        truncation_character = "...",
-      },
-      close_if_last_window = true,
-      popup_border_style = "rounded",
-      enable_git_status = true,
-      enable_diagnostics = true,
-      open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
-      open_files_using_relative_paths = false,
-      sort_case_insensitive = false,
-      sort_function = function(a, b)
-        if a.type == "directory" and b.type ~= "directory" then
-          return true
-        elseif a.type ~= "directory" and b.type == "directory" then
-          return false
-        end
-        return a.path < b.path
-      end,
-      default_container_configs = {
-        indent = {
-          indent_size = 4,
-        },
-        container = {
-          enable_character_fade = true,
-        },
-        modified = {
-          symbol = " ",
-          highlight = "NeoTreeModified",
-        },
-        name = {
-          trailing_slash = false,
-          use_git_status_colors = true,
-          highlight = "NeoTreeFileName",
-        },
-        git_status = {
-          symbols = {
-            --- change type
-            added = " ",
-            modified = " ",
-            --- status type
-            untracked = " ",
-            ignored = "󰈉 ",
-            unstaged = "󱧙 ",
-            staged = "󱧘 ",
-            conflict = " ",
-          },
-        },
-      },
-      window = {
-        position = "left",
-        width = 32,
-      },
-      filesystem = {
-        hide_gitignore = true,
-        hide_by_name = {
-          "node_modules",
-        },
-      },
-    })
-  end,
-  lazy = false,
-  ---@module "neo-tree"
-  ---@type neotree.Config?
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		{
+			"s1n7ax/nvim-window-picker",
+			version = "2.*",
+			config = function()
+				require("window-picker").setup({
+					filter_rules = {
+						include_current_win = false,
+						autoselect_one = true,
+						bo = {
+							filetype = { "neo-tree", "neo-tree-popup", "notify" },
+							buftype = { "terminal", "quickfix" },
+						},
+					},
+				})
+			end,
+		},
+	},
+	config = function()
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = " ",
+				},
+			},
+		})
+		require("neo-tree").setup({
+			source_selector = {
+				truncation_character = "...",
+			},
+			close_if_last_window = true,
+			popup_border_style = "rounded",
+			enable_git_status = true,
+			enable_diagnostics = true,
+			open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+			open_files_using_relative_paths = false,
+			sort_case_insensitive = false,
+			sort_function = function(a, b)
+				if a.type == "directory" and b.type ~= "directory" then
+					return true
+				elseif a.type ~= "directory" and b.type == "directory" then
+					return false
+				end
+				return a.path < b.path
+			end,
+			default_container_configs = {
+				indent = {
+					indent_size = 4,
+				},
+				container = {
+					enable_character_fade = true,
+				},
+				modified = {
+					symbol = " ",
+					highlight = "NeoTreeModified",
+				},
+				name = {
+					trailing_slash = false,
+					use_git_status_colors = true,
+					highlight = "NeoTreeFileName",
+				},
+				git_status = {
+					symbols = {
+						--- change type
+						added = " ",
+						modified = " ",
+						--- status type
+						untracked = " ",
+						ignored = "󰈉 ",
+						unstaged = "󱧙 ",
+						staged = "󱧘 ",
+						conflict = " ",
+					},
+				},
+			},
+			window = {
+				position = "left",
+				width = 38,
+			},
+			filesystem = {
+				hide_gitignore = true,
+				hide_by_name = {
+					"node_modules",
+				},
+			},
+		})
+	end,
+	lazy = false,
+	---@module "neo-tree"
+	---@type neotree.Config?
 }
