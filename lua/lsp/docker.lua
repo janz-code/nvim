929d1c1e4682429e76051e5b964962398872be5f
janz-code <jczp_020401@hotmail.com>
2025-08-16 22:14:41 -0500

    new lsp activation architecture working fine by now, docker and docker compose support added

--- lua/lsp/docker.lua
+++ lua/lsp/docker.lua
@@ -1,13 +1,13 @@
 vim.api.nvim_create_autocmd("BufRead", {
-	pattern = { "*-compose.yml", "*-compose.yaml", ".dockerignore", "DockerFile" },
+	pattern = { "*-compose.yml", "*-compose.yaml", ".dockerignore", "Dockerfile" },
 	callback = function(args)
 		local bufnr = vim.api.nvim_get_current_buf()
 		local filename = vim.api.nvim_buf_get_name(bufnr)
 		if filename:match("-compose%.ya?ml$") then
 			vim.bo[args.buf].filetype = "yaml.docker-compose"
 			vim.lsp.start(require("config.lsp.docker_compose_ls"))
-			return
+		else
+			vim.lsp.start(require("config.lsp.dockerls"))
 		end
-		vim.lsp.start(require("config.lsp.docker_language_server"))
 	end,
 })
